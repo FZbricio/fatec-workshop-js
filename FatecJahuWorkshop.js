b0a4dedbd3ac1303fdb5800a084bb6e20e0e5ba0
FABRICIO ALVES DE SOUZA <0201392321021>
2024-09-10 10:37:29 -0300

    Adicionei a solução para o desafio

---
+++ FatecJahuWorkshop.js
@@ -0,0 +1,8 @@
+const x = 5;
+const y = 5;
+
+function somar() {
+  let z = x + y;
+  return z;
+}
+console.log(somar());
---
+++ Identificacao.md
@@ -0,0 +1,2 @@
+Fabricio Alves de Souza
+Curso: Desevolvimento de software multiplataforma - 3° Período
